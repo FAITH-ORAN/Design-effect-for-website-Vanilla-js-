8584802125e7f8650618e36fb65d2c6b897c1a7a
Faiza <berrichif0717@gmail.com>
2022-07-07 14:43:08 +0200

    ajout du projet 32 et 33

---
+++ project-35/main.js
@@ -0,0 +1,37 @@
+const imgs=document.getElementById("imgs");
+const leftBtn=document.getElementById("left");
+const rightBtn=document.getElementById("right");
+
+const img=document.querySelectorAll("#imgs img")
+console.log(rightBtn);
+let idx=0;
+let interval=setInterval(run,2000)
+
+function run() {
+    idx++;
+    changeImg()
+}
+function changeImg() {
+    if(idx>img.length-1){
+        idx=0
+    }else if(idx<0){
+        idx=img.length-1
+    }
+    imgs.style.transform=`translateX(${-idx*500}px)`;
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    interval=setInterval(run,2000)
+
+}
+rightBtn.addEventListener("click",()=>{
+    idx++;
+    changeImg()
+    resetInterval()
+})
+leftBtn.addEventListener("click",()=>{
+    idx--;
+    changeImg()
+    resetInterval()
+})
